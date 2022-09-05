98d0b4d6e4f9975ae6c52443b1afc3871ab3cc4a
mycodeinpeace <me@codeinpeace.com>
2022-09-05 17:26:34 +0300

    mssc-brewery - New Project

--- mssc-brewery/src/main/java/com/codeinpeace/msscbrewery/MsscBreweryApplication.java
+++ mssc-brewery/src/main/java/com/codeinpeace/msscbrewery/MsscBreweryApplication.java
@@ -1,4 +1,4 @@
-package guru.springframework.msscbrewery;
+package com.codeinpeace.msscbrewery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- mssc-brewery/src/main/java/guru/springframework/msscbrewery/services/BeerService.java
+++
@@ -1,9 +0,0 @@
-package guru.springframework.msscbrewery.services;
-
-import guru.springframework.msscbrewery.web.model.BeerDto;
-
-import java.util.UUID;
-
-public interface BeerService {
-    BeerDto getBeerById(UUID beerId);
-}

56d06449716d2a30d423e65688ba2b402b213178
mycodeinpeace <me@codeinpeace.com>
2022-09-06 17:40:42 +0300

    mssc-brewery - API Versioning Example

---
+++ mssc-brewery/src/main/java/com/codeinpeace/msscbrewery/services/v2/BeerServiceV2.java
@@ -0,0 +1,16 @@
+package com.codeinpeace.msscbrewery.services.v2;
+
+import com.codeinpeace.msscbrewery.web.model.v2.BeerDtoV2;
+
+import java.util.UUID;
+
+public interface BeerServiceV2 {
+
+    BeerDtoV2 getBeetById(UUID beerId);
+
+    BeerDtoV2 saveNewBeer(BeerDtoV2 beerDto);
+
+    void updateBeer(UUID beerId, BeerDtoV2 beerDto);
+
+    void deleteById(UUID beerId);
+}
---
+++ mssc-brewery/src/main/java/com/codeinpeace/msscbrewery/web/model/v2/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package com.codeinpeace.msscbrewery.web.model.v2;
+
+public enum BeerStyleEnum {
+    LAGER, PISLSNER, ALE, STOUT, GOSE, IPA
+}

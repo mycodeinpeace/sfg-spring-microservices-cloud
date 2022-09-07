92aa3ccf43f0055d6cd1e032c02fce9ade4c0377
mycodeinpeace <me@codeinpeace.com>
2022-09-07 22:34:17 +0300

    mssc-beer-service - LC - Spring Data JPA Repositories

---
+++ mssc-beer-service/src/main/java/com/codeinpeace/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.codeinpeace.msscbeerservice.repositories;
+
+import com.codeinpeace.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}

2a2344e308f7d64183064f7e36eedcffef3b4dee
mycodeinpeace <me@codeinpeace.com>
2022-09-07 11:59:07 +0300

    mssc-brewery-client - Integration Test

---
+++ mssc-brewery-client/src/test/java/com/codeinpeace/msscbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,24 @@
+package com.codeinpeace.msscbreweryclient.web.client;
+
+import com.codeinpeace.msscbreweryclient.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient client;
+
+    @Test
+    void getBeerById() {
+        BeerDto dto = client.getBeerById(UUID.randomUUID());
+
+        assertNotNull(dto);
+    }
+}

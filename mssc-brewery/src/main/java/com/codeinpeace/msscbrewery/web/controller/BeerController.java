3098452f7eef9386dc5377e873ccf4a41682322c
mycodeinpeace <me@codeinpeace.com>
2022-09-08 01:05:18 +0300

    mssc-brewery - Bean Validation Implementation

--- mssc-brewery/src/main/java/com/codeinpeace/msscbrewery/web/controller/BeerController.java
+++ mssc-brewery/src/main/java/com/codeinpeace/msscbrewery/web/controller/BeerController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @Deprecated
@@ -26,7 +27,7 @@ public class BeerController {
     }
 
     @PostMapping
-    public  ResponseEntity handlePost(@RequestBody BeerDto beerDto) {
+    public  ResponseEntity handlePost(@Valid @RequestBody BeerDto beerDto) {
         BeerDto saveDto = beerService.saveNewBeer(beerDto);
         HttpHeaders headers = new HttpHeaders();
         headers.add("Location", "/api/v1/beer/" + saveDto.getId().toString());
@@ -35,7 +36,7 @@ public class BeerController {
     }
 
     @PutMapping({"/{beerId}"})
-    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, @Valid @RequestBody BeerDto beerDto) {
         beerService.updateBeer(beerId, beerDto);
 
         return new ResponseEntity(HttpStatus.NO_CONTENT);

98a98972f119a227de25ee9a4229e3f956a2b5a9
mycodeinpeace <me@codeinpeace.com>
2022-09-09 00:26:54 +0300

    mssc-brewery - Example of using Project Lombok and MapStruct

---
+++ mssc-brewery/src/main/java/com/codeinpeace/msscbrewery/web/mapper/BeerMapper.java
@@ -0,0 +1,13 @@
+package com.codeinpeace.msscbrewery.web.mapper;
+
+import com.codeinpeace.msscbrewery.domain.Beer;
+import com.codeinpeace.msscbrewery.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface BeerMapper {
+
+    BeerDto beerToBeerDto(Beer beer);
+
+    Beer beerDtoToBeer(BeerDto dto);
+}

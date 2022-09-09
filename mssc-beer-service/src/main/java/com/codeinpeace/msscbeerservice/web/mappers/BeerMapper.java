e06a2488e0f62b7ff994c7e95ee4993a40e6f247
mycodeinpeace <me@codeinpeace.com>
2022-09-09 13:17:03 +0300

    mssc-beer-service - LC - Implementing MapStruct

---
+++ mssc-beer-service/src/main/java/com/codeinpeace/msscbeerservice/web/mappers/BeerMapper.java
@@ -0,0 +1,13 @@
+package com.codeinpeace.msscbeerservice.web.mappers;
+
+import com.codeinpeace.msscbeerservice.domain.Beer;
+import com.codeinpeace.msscbeerservice.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+@Mapper(uses = {DateMapper.class})
+public interface BeerMapper {
+
+    BeerDto BeerToBeerDto(Beer beer);
+
+    Beer BeerDtoToBeer(BeerDto dto);
+}

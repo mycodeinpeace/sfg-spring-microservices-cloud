32244667bf58134b4758fa2b4abeebdda6506b41
mycodeinpeace <me@codeinpeace.com>
2022-09-13 23:09:33 +0300

    mssc-beer-inventory-service - Initial project

---
+++ mssc-beer-inventory-service/src/main/java/com/codeinpeace/beer/inventory/service/web/mappers/BeerInventoryMapper.java
@@ -0,0 +1,13 @@
+package com.codeinpeace.beer.inventory.service.web.mappers;
+
+import com.codeinpeace.beer.inventory.service.domain.BeerInventory;
+import com.codeinpeace.beer.inventory.service.web.model.BeerInventoryDto;
+import org.mapstruct.Mapper;
+
+@Mapper(uses = {DateMapper.class})
+public interface BeerInventoryMapper {
+
+    BeerInventory beerInventoryDtoToBeerInventory(BeerInventoryDto beerInventoryDTO);
+
+    BeerInventoryDto beerInventoryToBeerInventoryDto(BeerInventory beerInventory);
+}

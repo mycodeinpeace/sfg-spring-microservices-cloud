e5f69a2e45f96b79a863b8117826f749626005b1
mycodeinpeace <me@codeinpeace.com>
2022-09-16 16:19:11 +0300

    mssc-beer-service - Create Get Beer by UPC Endpoint

--- mssc-beer-service/src/main/java/com/codeinpeace/msscbeerservice/services/BeerServiceImpl.java
+++ mssc-beer-service/src/main/java/com/codeinpeace/msscbeerservice/services/BeerServiceImpl.java
@@ -97,4 +97,11 @@ public class BeerServiceImpl implements BeerService {
 
         return beerMapper.beerToBeerDto(beerRepository.save(beer));
     }
+
+    @Cacheable(cacheNames = "beerUpcCache")
+    @Override
+    public BeerDto getByUpc(String upc) {
+        System.out.println("Service call getByUpc");
+        return beerMapper.beerToBeerDto(beerRepository.findByUpc(upc));
+    }
 }

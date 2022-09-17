9c7b4c952b143ffd3aa5c38c71825c7e36dda697
mycodeinpeace <me@codeinpeace.com>
2022-09-17 22:36:34 +0300

    mssc-beer-inventory-service - Fix UUID issue

--- mssc-beer-inventory-service/src/main/java/com/codeinpeace/beer/inventory/service/domain/BeerInventory.java
+++ mssc-beer-inventory-service/src/main/java/com/codeinpeace/beer/inventory/service/domain/BeerInventory.java
@@ -4,7 +4,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import java.sql.Timestamp;
 import java.util.UUID;
@@ -13,7 +15,7 @@ import java.util.UUID;
 @Setter
 @NoArgsConstructor
 @Entity
-public class BeerInventory extends BaseEntity{
+public class BeerInventory extends BaseEntity {
 
     @Builder
     public BeerInventory(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate, UUID beerId,
@@ -24,6 +26,8 @@ public class BeerInventory extends BaseEntity{
         this.quantityOnHand = quantityOnHand;
     }
 
+    @Type(type="org.hibernate.type.UUIDCharType")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false )
     private UUID beerId;
     private String upc;
     private Integer quantityOnHand = 0;

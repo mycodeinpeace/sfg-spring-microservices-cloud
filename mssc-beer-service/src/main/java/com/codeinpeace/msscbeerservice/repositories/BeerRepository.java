f32caa64683db1d4f994aa04a4bad60c2c6a652b
mycodeinpeace <me@codeinpeace.com>
2022-09-21 00:50:39 +0300

    mssc-beer-service - Create Brewing Service

--- mssc-beer-service/src/main/java/com/codeinpeace/msscbeerservice/repositories/BeerRepository.java
+++ mssc-beer-service/src/main/java/com/codeinpeace/msscbeerservice/repositories/BeerRepository.java
@@ -4,11 +4,12 @@ import com.codeinpeace.msscbeerservice.domain.Beer;
 import com.codeinpeace.msscbeerservice.web.model.BeerStyleEnum;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.UUID;
 
-public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
     Page<Beer> findAllByBeerName(String beerName, Pageable pageable);
 
     Page<Beer> findAllByBeerStyle(BeerStyleEnum beerStyle, Pageable pageable);

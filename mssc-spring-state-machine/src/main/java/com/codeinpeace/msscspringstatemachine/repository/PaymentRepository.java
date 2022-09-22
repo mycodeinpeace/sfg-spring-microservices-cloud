acba99a7376b06231bd38ceb8426bb8b2a5bea51
mycodeinpeace <me@codeinpeace.com>
2022-09-22 22:16:55 +0300

    mssc-spring-state-machine - Spring Data JPA Configuration

---
+++ mssc-spring-state-machine/src/main/java/com/codeinpeace/msscspringstatemachine/repository/PaymentRepository.java
@@ -0,0 +1,7 @@
+package com.codeinpeace.msscspringstatemachine.repository;
+
+import com.codeinpeace.msscspringstatemachine.domain.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+}

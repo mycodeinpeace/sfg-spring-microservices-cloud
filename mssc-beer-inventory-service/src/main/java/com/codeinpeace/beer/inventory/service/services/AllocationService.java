48d53e8470ae60921f9c55b03f52a768abf106b9
mycodeinpeace <me@codeinpeace.com>
2022-09-25 14:39:56 +0300

    mssc-beer-inventory-service - Initiate Allocate Order

---
+++ mssc-beer-inventory-service/src/main/java/com/codeinpeace/beer/inventory/service/services/AllocationService.java
@@ -0,0 +1,8 @@
+package com.codeinpeace.beer.inventory.service.services;
+
+import com.codeinpeace.brewery.model.BeerOrderDto;
+
+public interface AllocationService {
+
+    Boolean allocateOrder(BeerOrderDto beerOrderDto);
+}

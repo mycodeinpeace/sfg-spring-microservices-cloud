4d58ed9e4e8a758a453c3f6536d146eadff752d0
mycodeinpeace <me@codeinpeace.com>
2022-09-25 14:58:34 +0300

    Assignment - Complete Allocation Message Flow

--- mssc-beer-order-service/src/main/java/com/codeinpeace/beer/order/service/services/BeerOrderManager.java
+++ mssc-beer-order-service/src/main/java/com/codeinpeace/beer/order/service/services/BeerOrderManager.java
@@ -1,6 +1,7 @@
 package com.codeinpeace.beer.order.service.services;
 
 import com.codeinpeace.beer.order.service.domain.BeerOrder;
+import com.codeinpeace.brewery.model.BeerOrderDto;
 
 import java.util.UUID;
 
@@ -9,4 +10,10 @@ public interface BeerOrderManager {
     BeerOrder newBeerOrder(BeerOrder beerOrder);
 
     void processValidationResult(UUID beerOrderId, Boolean isValid);
+
+    void beerOrderAllocationPassed(BeerOrderDto beerOrder);
+
+    void beerOrderAllocationPendingInventory(BeerOrderDto beerOrder);
+
+    void beerOrderAllocationFailed(BeerOrderDto beerOrder);
 }
--- mssc-beer-order-service/src/main/java/com/codeinpeace/brewery/model/BeerOrderLineDto.java
+++ mssc-beer-order-service/src/main/java/com/codeinpeace/brewery/model/BeerOrderLineDto.java
@@ -36,4 +36,6 @@ public class BeerOrderLineDto {
     private UUID beerId;
     private Integer orderQuantity = 0;
     private BigDecimal price;
+
+    private Integer quantityAllocated;
 }

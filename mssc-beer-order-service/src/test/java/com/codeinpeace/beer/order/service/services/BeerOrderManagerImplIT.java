eed69dc2e4e11b8434bcbbe4832150d2d470e3fa
Code In Peace <me@codeinpeace.com>
2022-09-27 21:48:14 +0300

    mssc-beer-order-service - Resolving Locking Failure

--- mssc-beer-order-service/src/test/java/com/codeinpeace/beer/order/service/services/BeerOrderManagerImplIT.java
+++ mssc-beer-order-service/src/test/java/com/codeinpeace/beer/order/service/services/BeerOrderManagerImplIT.java
@@ -89,11 +89,19 @@ public class BeerOrderManagerImplIT {
             assertEquals(BeerOrderStatusEnum.ALLOCATED, foundOrder.getOrderStatus());
         });
 
+        await().untilAsserted(() -> {
+            BeerOrder foundOrder = beerOrderRepository.findById(beerOrder.getId()).get();
+            BeerOrderLine line = foundOrder.getBeerOrderLines().iterator().next();
+            assertEquals(line.getOrderQuantity(), line.getQuantityAllocated());
+        });
+
         BeerOrder savedBeerOrder2 = beerOrderRepository.findById(savedBeerOrder.getId()).get();
 
-        assertNotNull(savedBeerOrder);
+        assertNotNull(savedBeerOrder2);
         assertEquals(BeerOrderStatusEnum.ALLOCATED, savedBeerOrder2.getOrderStatus());
-
+        savedBeerOrder2.getBeerOrderLines().forEach(line -> {
+            assertEquals(line.getOrderQuantity(), line.getQuantityAllocated());
+        });
     }
 
     public BeerOrder createBeerOrder(){
@@ -114,3 +122,4 @@ public class BeerOrderManagerImplIT {
         return beerOrder;
     }
 }
+

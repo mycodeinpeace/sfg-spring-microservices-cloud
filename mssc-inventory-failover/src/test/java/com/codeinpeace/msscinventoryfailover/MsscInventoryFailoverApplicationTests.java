01403aea44add3d5b52586aa99a4b0646430ded6
Code In Peace <me@codeinpeace.com>
2022-10-07 19:32:07 +0300

    mssc-inventory-failover - Initial project

---
+++ mssc-inventory-failover/src/test/java/com/codeinpeace/msscinventoryfailover/MsscInventoryFailoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeinpeace.msscinventoryfailover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscInventoryFailoverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

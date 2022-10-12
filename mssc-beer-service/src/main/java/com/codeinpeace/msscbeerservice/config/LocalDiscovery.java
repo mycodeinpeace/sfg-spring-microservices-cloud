f25cd8a9437bfd278849e3d10aecc05413bcb3f4
Code In Peace <me@codeinpeace.com>
2022-10-13 00:14:09 +0300

    Fix: InventoryFailoverFeignClient issue
    Error starting ApplicationContext. To display the conditions report re-run your application with 'debug' enabled.
    2022-10-12 23:16:38.593 ERROR 16684 --- [           main] o.s.b.d.LoggingFailureAnalysisReporter   :
    
    ***************************
    APPLICATION FAILED TO START
    ***************************
    
    Description:
    
    Parameter 0 of constructor in com.codeinpeace.msscbeerservice.services.inventory.InventoryServiceFeignClientFailover required a bean of type 'com.codeinpeace.msscbeerservice.services.inventory.InventoryFailoverFeignClient' that could not be found.
    
    Action:
    
    Consider defining a bean of type 'com.codeinpeace.msscbeerservice.services.inventory.InventoryFailoverFeignClient' in your configuration.
    
    Process finished with exit code 1

--- mssc-beer-service/src/main/java/com/codeinpeace/msscbeerservice/config/LocalDiscovery.java
+++ mssc-beer-service/src/main/java/com/codeinpeace/msscbeerservice/config/LocalDiscovery.java
@@ -1,14 +1,11 @@
 package com.codeinpeace.msscbeerservice.config;
 
-import com.codeinpeace.msscbeerservice.services.inventory.InventoryServiceFeignClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Profile("local-discovery")
 @EnableDiscoveryClient
-@EnableFeignClients(clients = InventoryServiceFeignClient.class)
 @Configuration
 public class LocalDiscovery {
 }
